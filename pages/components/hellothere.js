818a436b847fc9295ff818ee06eecc7677d9c175
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-03-28 13:25:06 -0300

    newcomponent test

---
+++ pages/components/hellothere.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+
+export default function hellothere() {
+  return (
+    <>
+      <h1 className={styles.title}>Hello There...</h1>
+      <h1 className={styles.title}>General Kenobi...</h1>
+    </>
+  );
+}
